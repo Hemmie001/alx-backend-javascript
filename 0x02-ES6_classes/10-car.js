40aff6635d9acb97746882cac6ccfcf659bf2c3b
Hemmie001 <emmanuelakej@gmail.com>
2024-01-05 23:27:05 +0000

    correction 10

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,9 +1,41 @@
-export default function appendToEachArrayValue(array, appendString) {
-  const newArray = [];
-  // eslint-disable-next-line no-restricted-syntax
-  for (const value of array) {
-    newArray.push(appendString + value);
+export default class Car {
+  constructor(brand, motor, color) {
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
   }
 
-  return newArray;
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(value) {
+    this._brand = value;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(value) {
+    this._motor = value;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(value) {
+    this._color = value;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+
+    return new Species();
+  }
 }
